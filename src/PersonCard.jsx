42463ccf9a67a0fbf7eaa369dcba783cb23d605c
MaiTran41 <maitran.040196@gmail.com>
2025-04-09 23:30:13 +0300

    add years calculation fnc

--- src/PersonCard.jsx
+++ src/PersonCard.jsx
@@ -1,23 +1,57 @@
-import './Person.css'
+import "./Person.css";
+
+const calculateWorkingYears = (startDateStr) => {
+  const today = new Date();
+
+  const startDate = new Date(startDateStr);
+  const workingYears = today.getFullYear() - startDate.getFullYear();
+
+  return workingYears;
+};
+const calculateWorkingMonths = (startDateStr) => {
+  const today = new Date();
+
+  const startDate = new Date(startDateStr);
+  const workingMonths = today.getMonth() - startDate.getFullYear();
+
+  return workingYears;
+};
+
+const shouldRenderRecognitionMsg = (startDate) => {
+  //   const yearsToGetRecognition = new Set([5, 10, 15]);
+  //   return yearsToGetRecognition.has(workingYears);
+
+  const workingYears = calculateWorkingYears(startDate);
+
+  return workingYears === 5 || workingYears === 10 || workingYears === 15;
+};
+const shouldRenderProbationMsg = (startDate) => {
+  //   const yearsToGetRecognition = new Set([5, 10, 15]);
+  //   return yearsToGetRecognition.has(workingYears);
+
+  return workingYears <= 0.5;
+};
 
 const PersonCard = (props) => {
+  return (
+    <div className="person-block">
+      <p>ID: {props.id}</p>
+      <p>Name: {props.name}</p>
+      <p>Title: {props.title}</p>
+      <p>Salary: {props.salary}€</p>
+      <p>Phone: {props.phone}</p>
+      <p>Email: {props.email}</p>
+      <p>Animal: {props.animal}</p>
+      <p>Start Date: {props.startDate}</p>
+      <p>Location: {props.location}</p>
+      <p>Department: {props.department}</p>
+      <p>Skills: {props.skills.join(", ")}</p>
 
-
-    
-return (
-    <div className='person-block'>
-    <p>ID: {props.id}</p>
-    <p>Name: {props.name}</p>
-    <p>Title: {props.title}</p>
-    <p>Salary: {props.salary}€</p>
-    <p>Phone: {props.phone}</p>
-    <p>Email: {props.email}</p>
-    <p>Animal: {props.animal}</p>
-    <p>Start Date: {props.startDate}</p>
-    <p>Location: {props.location}</p>
-    <p>Department: {props.department}</p>
-    <p>Skills: {props.skills.join(', ')}</p>
+      {shouldRenderRecognitionMsg(props.startDate) && (
+        <p>🎉 Schedule recognition meeting</p>
+      )}
+      {/* <p>🔔 Schedule probation review</p> */}
     </div>
-)
-}
-export default PersonCard
+  );
+};
+export default PersonCard;
--- src/PersonList.jsx
+++ src/PersonList.jsx
@@ -1,15 +1,17 @@
-import PersonCard from "./PersonCard"
-import { employees } from "./employee"
+import PersonCard from "./PersonCard";
+import { employees } from "./employee";
 
-const PersonList = ( ) => {
-    return (
-        <>
-        <h2>Employee List</h2>
-        <div className="employee-card-container">
-       { employees.map((employee) => <PersonCard key={employee.id} {...employee} /> )}
-        </div>
-        </>
-    )
-}
+const PersonList = () => {
+  return (
+    <>
+      <h2>Employee List</h2>
+      <div className="employee-card-container">
+        {employees.map((employee) => (
+          <PersonCard key={employee.id} {...employee} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default PersonList
+export default PersonList;
