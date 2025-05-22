bbdaa6792132038ca61de0519557d2eaa54dd673
MaiTran41 <maitran.040196@gmail.com>
2025-05-22 03:25:00 +0300

    add searching feature in EmployeeList

--- src/pages/PersonList/PersonList.jsx
+++ src/pages/PersonList/PersonList.jsx
@@ -1,19 +1,57 @@
-import styles from "../PersonList/PersonList.module.css";
+import styles from "./PersonList.module.css";
 import PersonCard from "../../components/PersonCard/PersonCard";
+import { useState } from "react";
 
 const PersonList = ({ employeesData, onSalaryChange }) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchHandle = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const filteredEmployees = employeesData.filter((employee) => {
+    const search = searchValue.toLowerCase();
+
+    const matchesSearch =
+      employee.name.trim().toLowerCase().includes(search) ||
+      employee.title.trim().toLowerCase().includes(search) ||
+      employee.department.trim().toLowerCase().includes(search);
+
+    return matchesSearch;
+  });
+
   return (
     <>
       <div className={styles.employeeListContainer}>
         <h2 className={styles.heading}>Employee List</h2>
+
+        <div className={styles.searchContainer}>
+          <label htmlFor="search">Search</label>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            value={searchValue}
+            onChange={searchHandle}
+          />
+        </div>
+
         <div className={styles.employeeListWrapper}>
-          {employeesData.map((employee) => (
-            <PersonCard
-              key={employee.id}
-              {...employee}
-              onSalaryChange={(id, newSalary) => onSalaryChange(id, newSalary)}
-            />
-          ))}
+          {filteredEmployees.length > 0 ? (
+            filteredEmployees.map((employee) => (
+              <PersonCard
+                key={employee.id}
+                {...employee}
+                onSalaryChange={(id, newSalary) =>
+                  onSalaryChange(id, newSalary)
+                }
+              />
+            ))
+          ) : (
+            <p className={styles.searchMessage}>
+              Plot twist: Nothing found! 🎬 The search continues in episode 2...
+            </p>
+          )}
         </div>
       </div>
     </>
